9835365dbafa8565871a6856d40e7fb74bb85f18
shivanisah <shivanisah66@gmail.com>
2024-01-19 12:58:13 +0530

    clock

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateClock() {
+  const clockElement = document.getElementById('clock');
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+
+  clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+updateClock();
+
+setInterval(updateClock, 1000);
